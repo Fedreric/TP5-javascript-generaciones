f7144ad329f5e4c05e9bdfcccee638190218ea81
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-02 15:56:27 -0300

    se realiza la toma de datos desde los inputs y se los muestra en pantalla

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const form = document.getElementById('formulario');
+const mostrarEnPantalla = document.getElementById('mostrarEnPantalla');
+form.addEventListener('submit', crearPersona);
 class Persona {
     #nombre;
     #añoNacimiento;
@@ -6,13 +9,13 @@ class Persona {
     #sexo;
     #peso;
     #altura;
-    constructor( nombre,añoNacimiento,edad, DNI,sexo,peso,altura) {
+    constructor(nombre, añoNacimiento, edad, DNI, sexo, peso, altura) {
         this.#nombre = nombre;
         this.#añoNacimiento = añoNacimiento;
         this.#edad = edad;
         this.#DNI = DNI;
         this.#sexo = sexo;
-        this.#peso =peso;
+        this.#peso = peso;
         this.#altura = altura;
     }
     get nombre() { return this.#nombre; }
@@ -31,8 +34,8 @@ class Persona {
     set peso(newPeso) { this.#peso = newPeso; }
     set altura(newAltura) { this.#altura = newAltura; }
 
-    mostrarDatos(){
-        document.write(`<ul>
+    mostrarDatos() {
+        mostrarEnPantalla.innerHTML = (`<ul>
         <li>Nombre: ${this.nombre}</li>
         <li>Año de nacimiento: ${this.#añoNacimiento}</li>
         <li>Edad: ${this.edad}</li>
@@ -42,33 +45,33 @@ class Persona {
         <li>Altura: ${this.altura}</li>
         </ul>`);
     }
-    mostrarGeneracion(){
+    mostrarGeneracion() {
         let añoNac = this.añoNacimiento;
         let generacion = '';
         let rasgo = '';
 
-        if(añoNac>=1994 && añoNac<=2010){
+        if (añoNac >= 1994 && añoNac <= 2010) {
             generacion = 'Z';
             rasgo = 'Irreverencia';
         }
-        else if(añoNac>=1981 && añoNac<=1993){
+        else if (añoNac >= 1981 && añoNac <= 1993) {
             generacion = 'Y';
             rasgo = 'Frustración';
         }
-        else if(añoNac>=1969 && añoNac<=1980){
+        else if (añoNac >= 1969 && añoNac <= 1980) {
             generacion = 'X';
             rasgo = 'Obsesión por el éxito';
-        
-        }else if(añoNac>=1949 && añoNac<=1968){
+
+        } else if (añoNac >= 1949 && añoNac <= 1968) {
             generacion = 'Baby Boom';
             rasgo = 'Ambición';
-        
-        }else if(añoNac>=1930 && añoNac<=1948){
+
+        } else if (añoNac >= 1930 && añoNac <= 1948) {
             generacion = 'Silent Generation';
             rasgo = 'Austeridad';
         }
 
-        switch(generacion){
+        switch (generacion) {
             case 'Z':
                 document.write(`${this.nombre} pertenece a la generación:<span> ${generacion}</span> y su rasgo principal es la: <span>${rasgo}</span>`);
                 break;
@@ -85,17 +88,32 @@ class Persona {
                 document.write(`${this.nombre} pertenece a la generación:<span> ${generacion}</span> y su rasgo principal es la: <span>${rasgo}</span>`);
                 break;
             default:
-            document.write(`${this.nombre} no pertenece a ninguna generación`);
-            break;
+                document.write(`${this.nombre} no pertenece a ninguna generación`);
+                break;
         }
     }
-    mayorDeEdad(){
-        if(this.edad >= 18){
+    mayorDeEdad() {
+        if (this.edad >= 18) {
             document.write(`<p>${this.nombre} de ${this.edad} años, es mayo de edad.</p>`);
-        }else{
+        } else {
             document.write(`<p>${this.nombre} de ${this.edad} años, es menor de edad.</p>`);
         }
     }
+}
+
+function crearPersona(e) {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre');
+    const añoNacimiento = document.getElementById('añoNacimiento');
+    const edad = document.getElementById('edad');
+    const dni = document.getElementById('dni');
+    const sexo = document.getElementById('sexo');
+    const peso = document.getElementById('peso');
+    const altura = document.getElementById('altura');
+
+    const persona = new Persona(nombre.value, añoNacimiento.value, edad.value, dni.value, sexo.value, peso.value, altura.value);
+    persona.mostrarDatos();
+    console.log(persona);
 }
 
 // let federico = new Persona('Federico',2000,22,43204867,'H','70Kg',1.75);
